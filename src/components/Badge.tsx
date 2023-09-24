f2573c363863bf0423c23aa0c164cf2cba559beb
yezi <yehaowei20060411@qq.com>
2023-09-25 06:43:51 +0800

    feat: 优化 Badge 组件

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -5,14 +5,22 @@ import Text from "./typography/Text";
 interface Props {
   children: ComponentChildren;
   className?: string;
-  color: string;
+  textColor?: string;
+  backgroundColor: string;
+  rounded?: string;
 }
 
-export default function Badge({ children, className, color }: Props) {
+export default function Badge({
+  children,
+  className,
+  textColor,
+  backgroundColor,
+  rounded = "rounded",
+}: Props) {
   return (
     <Text
-      className={clsx(className, "rounded-full w-fit py-1 px-3")}
-      color={color}
+      className={clsx(className, "w-fit py-1 px-2", backgroundColor, rounded)}
+      color={textColor}
       isSmall
       isBold
       isInline
--- stories/Badge.stories.tsx
+++ stories/Badge.stories.tsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import type { Meta } from "@storybook/preact";
-import { Badge } from "../src/main";
+import { Badge, Text } from "../src/main";
 
 const meta: Meta<typeof Badge> = {
-  component: Badge,
+  component: (props) => (
+    <Text>
+      There is a badge：
+      <Badge {...props} />
+    </Text>
+  ),
   args: {
-    children: "Badge",
+    children: "测试 Badge",
     className: undefined,
-    color: "bg-green-300",
+    textColor: "text-green-700 dark:text-green-300",
+    backgroundColor: "bg-green-200 dark:bg-green-800",
+    rounded: undefined,
   },
   argTypes: {
     className: {
+      control: "text",
+    },
+    rounded: {
       control: "text",
     },
   },
