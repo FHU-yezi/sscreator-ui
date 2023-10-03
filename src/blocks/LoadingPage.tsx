63ad160abc11c84401a06102aa6740d13d500c05
yezi <yehaowei20060411@qq.com>
2023-10-03 09:11:40 +0800

    feat: 修改加载页图标

--- src/blocks/LoadingPage.tsx
+++ src/blocks/LoadingPage.tsx
@@ -1,10 +1,13 @@
-import LoadingIcon from "../components/LoadingIcon";
+import { BiLoader } from "react-icons/bi";
 import Center from "../components/layout/Center";
+import Text from "../components/typography/Text";
 
 export default function LoadingPage() {
   return (
     <Center className="h-screen">
-      <LoadingIcon size={64} />
+      <Text className="motion-safe:animate-spin">
+        <BiLoader size={64} />
+      </Text>
     </Center>
   );
 }
